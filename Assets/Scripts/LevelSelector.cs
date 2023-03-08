85fc9df7b1ddee8f2d947421309a64c4a968058a
leodang1996 <leodang1996@gmail.com>
2023-03-08 15:34:31 +0700

    update

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public int level;
+    public Text levelText;
+    // Start is called before the first frame update
+    void Start()
+    {
+       levelText.text = level.ToString();
+    }
+
+    public void OpenScene() 
+    {
+        SceneManager.LoadScene("Level " + level.ToString());
+    }
+
+
+}
